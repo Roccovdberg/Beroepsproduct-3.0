02dda96404f532c2ef5d2cba25df10ee9ce5f9c3
Roccovdberg <150138783+Roccovdberg@users.noreply.github.com>
2024-02-07 11:43:49 +0100

    Jar file toegevoegd en readme geschreven.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Beroepsproduct
- 
+# Implementatieplan MyTurn
+
+## Inleiding
+Dit systeem is ontwikkeld omdat veel mensen producten hebben die ze bereid zijn uit te lenen, maar niet weten aan wie. Daarom is er een systeem gemaakt waar mensen producten kunnen toevoegen, bewerken, bekijken en verwijderen.
+
+## Doelstellingen
+- Gebruikers kunnen producten toevoegen, bewerken en verwijderen.
+- Alle wijzigingen aan producten worden bijgewerkt in een MySQL-database.
+
+## Technologieën
+- Programmeertalen: JavaFX, MySQL.
+- Database: MySQL voor het opslaan van producten, contracten en accounts.
+- Versiebeheer: Git en GitHub voor samenwerking en versiebeheer.
+- Testen: Unittests zijn uitgevoerd.
+
+## Stappenplan
+1. **Inloggen**: Gebruikers loggen in op het systeem of maken een nieuw account aan. Accounts worden toegevoegd aan de database.
+2. **Homepagina**: Na het inloggen komen gebruikers op de homepagina terecht. Hier kunnen ze producten bekijken en nieuwe toevoegen.
+3. **Product toevoegen**: Gebruikers kunnen via een invulformulier informatie over een product invoeren. Dit wordt toegevoegd aan de database en getoond op het scherm.
+4. **Contract maken**: Als een gebruiker interesse heeft in een product, kunnen ze samen een contract opstellen via een formulier. Dit wordt opgeslagen in de database.
+5. **Product bewerken**: Gebruikers kunnen op een product klikken om informatie te bewerken. Alle wijzigingen worden automatisch bijgewerkt in de database.
+6. **Product verwijderen**: Als een gebruiker besluit een product niet meer te willen uitlenen, kunnen ze het verwijderen. Dit wordt ook verwijderd uit de database.
+
+## Conclusie
+Het implementatieplan voor MyTurn biedt een gestructureerde aanpak voor het ontwikkelen van een systeem waarin gebruikers producten kunnen beheren voor verhuur aan anderen. MyTurn zal bijdragen aan het vergemakkelijken van het delen van producten binnen gemeenschappen, terwijl gebruikers tegelijkertijd de controle behouden over hun verhuurprocessen.
--- src/main/java/com/example/beroepsproduct/HelloApplication.java
+++ src/main/java/com/example/beroepsproduct/HelloApplication.java
@@ -17,59 +17,62 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
-    // Scherm aanmaken
 
-    public void start(Stage stage) throws IOException {
-        GridPane loginGridPane = new GridPane(); // GridPane voor inlogscherm
-        loginGridPane.setPadding(new Insets(20));
-        loginGridPane.setVgap(10);
-        loginGridPane.setAlignment(Pos.CENTER);
+        @Override
+        // Scherm aanmaken
 
-        GridPane mainGridPane = new GridPane(); // GridPane voor hoofdscherm
-        mainGridPane.setPadding(new Insets(20));
-        mainGridPane.setVgap(10);
-        mainGridPane.setAlignment(Pos.CENTER);
+        public void start (Stage stage) throws IOException {
+            GridPane loginGridPane = new GridPane(); // GridPane voor inlogscherm
+            loginGridPane.setPadding(new Insets(20));
+            loginGridPane.setVgap(10);
+            loginGridPane.setAlignment(Pos.CENTER);
 
-        Label usernameLabel = new Label("Gebruikersnaam:");
-        TextField usernameInput = new TextField();
+            GridPane mainGridPane = new GridPane(); // GridPane voor hoofdscherm
+            mainGridPane.setPadding(new Insets(20));
+            mainGridPane.setVgap(10);
+            mainGridPane.setAlignment(Pos.CENTER);
 
-        Label passwordLabel = new Label("Wachtwoord:");
-        PasswordField passwordInput = new PasswordField();
+            Label usernameLabel = new Label("Gebruikersnaam:");
+            TextField usernameInput = new TextField();
 
-        Button inloggen = new Button("Maak account aan");
-        inloggen.setOnAction(e -> {
-            Login login = new Login(stage);
-            Scene loginScene = new Scene(login.getScene().getRoot(), 800, 600);
-            stage.setScene(loginScene);
+            Label passwordLabel = new Label("Wachtwoord:");
+            PasswordField passwordInput = new PasswordField();
+
+            Button inloggen = new Button("Maak account aan");
+            inloggen.setOnAction(e -> {
+                Login login = new Login(stage);
+                Scene loginScene = new Scene(login.getScene().getRoot(), 800, 600);
+                stage.setScene(loginScene);
+                stage.show();
+            });
+            inloggen.setCursor(Cursor.HAND);
+
+            Button loginButton = new Button("Inloggen");
+            //oginButton.setOnAction(e -> handleLogin(usernameInput.getText(), passwordInput.getText(), new Homescreen(stage)));
+            loginButton.setCursor(Cursor.HAND);
+
+            Button naarHomescreen = new Button("Homepage!");
+            naarHomescreen.setOnAction(e -> {
+                Homescreen homescreen = new Homescreen(stage);
+            });
+            naarHomescreen.setCursor(Cursor.HAND);
+
+            loginGridPane.add(usernameLabel, 0, 0);
+            loginGridPane.add(usernameInput, 1, 0);
+            loginGridPane.add(passwordLabel, 0, 1);
+            loginGridPane.add(passwordInput, 1, 1);
+            loginGridPane.add(inloggen, 0, 2);
+            loginGridPane.add(loginButton, 1, 2);
+            loginGridPane.add(naarHomescreen, 0, 3);
+
+            Scene loginScene = new Scene(loginGridPane, 800, 600); // Inlogscene
+
+            stage.setTitle("MyTurn");
+            stage.setScene(loginScene); // Stel inlogscene in als begin scene
             stage.show();
-        });
-        inloggen.setCursor(Cursor.HAND);
+        }
 
-        Button loginButton = new Button("Inloggen");
-        //oginButton.setOnAction(e -> handleLogin(usernameInput.getText(), passwordInput.getText(), new Homescreen(stage)));
-        loginButton.setCursor(Cursor.HAND);
-
-        Button naarHomescreen = new Button("Homepage!");
-        naarHomescreen.setOnAction(e -> {
-            Homescreen homescreen = new Homescreen(stage);
-        });
-        naarHomescreen.setCursor(Cursor.HAND);
-
-        loginGridPane.add(usernameLabel, 0, 0);
-        loginGridPane.add(usernameInput, 1, 0);
-        loginGridPane.add(passwordLabel, 0, 1);
-        loginGridPane.add(passwordInput, 1, 1);
-        loginGridPane.add(inloggen, 0, 2);
-        loginGridPane.add(loginButton, 1, 2);
-        loginGridPane.add(naarHomescreen, 0, 3);
-
-        Scene loginScene = new Scene(loginGridPane, 800, 600); // Inlogscene
-
-        stage.setTitle("MyTurn");
-        stage.setScene(loginScene); // Stel inlogscene in als begin scene
-        stage.show();
+    public static void main(String[] args) {
+        launch();
     }
-
-
 }
